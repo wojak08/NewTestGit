c5e4b4cc28c2119f62e10cd5d0346a4922b8324c
Fifi <wojak08@me.com>
2020-07-02 18:36:58 +0200

    Initial Commit

---
+++ NewTestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewTestGit
+//
+//  Created by Filip Wojcieszak on 02/07/2020.
+//  Copyright © 2020 Filip Wojcieszak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
